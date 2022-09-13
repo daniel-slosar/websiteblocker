2235758498d15d7e9204adaeee20afadb7aa53bf
Daniel <46535297+daniel-slosar@users.noreply.github.com>
2022-09-13 21:19:31 +0200

    Added display text file function

--- web_blocker.py
+++ web_blocker.py
@@ -1,15 +1,27 @@
 import PySimpleGUI as sg
 import requests
+import os
+
+# hosts_file = "C:\Windows\System32\drivers\etc\hosts"
 
 ip_add = "127.0.0.1"
-sites=[]
+
+def display_hosts():
+    try:
+        hosts = open('hosts.txt','r') #should be a path to hosts file
+        txt = hosts.read()
+        sg.popup_scrolled(txt)
+        
+    except Exception as e:
+        sg.popup_error(e)   
+
 
 def main_window():
     layout = [
         [sg.Text("Website Blocker", font="Arial 14 bold",justification='l')],
         [sg.Text("Website: ", font="Arial 12"),sg.Input(font="Arial 12",key="-INPUT-") ,sg.Button("Check website",font="Arial 12")],
         [sg.Button("Block website",key='-BLOCK-',visible=False,font="Arial 12"),sg.Button("Unblock",key='-UNBLOCK-',visible=False,font="Arial 12")],
-        [sg.Button("Display file",font='Arial 11 bold'),sg.Button("Next",'center',font='Arial 11 bold')]]
+        [sg.Button("Display file",font='Arial 11 bold'),sg.Button("Next",'center',font='Arial 11 bold')],
         [sg.Exit(button_color=("white","red"),font='Arial 11 bold')]
     ]
 
