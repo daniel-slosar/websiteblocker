586ba0ec44353cf28903e2b8c11a84e569ea556f
Daniel <46535297+daniel-slosar@users.noreply.github.com>
2022-09-13 21:47:22 +0200

    Centered elements

--- web_blocker.py
+++ web_blocker.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 import requests
-import os
+
 
 # hosts_file = "C:\Windows\System32\drivers\etc\hosts"
 
@@ -17,15 +17,17 @@ def display_hosts():
 
 
 def main_window():
+    column_to_be_centered = [
+        [sg.Button("Block website",key='-BLOCK-',visible=False,font="Arial 12"),sg.Button("Unblock",key='-UNBLOCK-',visible=False,font="Arial 12")],
+        [sg.Button("Display file",font='Arial 11 bold'),sg.Button("Next",'center',font='Arial 11 bold')]]
     layout = [
         [sg.Text("Website Blocker", font="Arial 14 bold",justification='l')],
-        [sg.Text("Website: ", font="Arial 12"),sg.Input(font="Arial 12",key="-INPUT-") ,sg.Button("Check website",font="Arial 12")],
-        [sg.Button("Block website",key='-BLOCK-',visible=False,font="Arial 12"),sg.Button("Unblock",key='-UNBLOCK-',visible=False,font="Arial 12")],
-        [sg.Button("Display file",font='Arial 11 bold'),sg.Button("Next",'center',font='Arial 11 bold')],
+        [sg.Text("Website: ", font="Arial 12"),sg.Input(font="Arial 12",key="-INPUT-") ,sg.Button("Check website",font="Arial 10")],
+        [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
         [sg.Exit(button_color=("white","red"),font='Arial 11 bold')]
     ]
 
-    window = sg.Window("Website Blocker", layout, size=(650, 200),grab_anywhere=True)
+    window = sg.Window("Website Blocker", layout, size=(690, 220),grab_anywhere=True)
 
     while True:
         event, values = window.read()
